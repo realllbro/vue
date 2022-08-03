242c8d1fb69f33711c1deccf1b115e34ca0a86bd
unknown <90885001@HITEJINRO.AD>
2022-08-03 17:32:22 +0900

    2022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-032022-08-03

--- src/03.vue-todo/src/App.vue
+++ src/03.vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="app">
         <TodoHeader></TodoHeader>
-        <TodoInput></TodoInput>
-        <TodoList></TodoList>
-        <TodoFooter></TodoFooter>
+        <TodoInput v-on:addTodo="addTodo"></TodoInput>
+        <TodoList  v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+        <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
     </div>
 </template>
 
@@ -13,11 +13,39 @@
 	import TodoList   from './components/TodoList.vue'
 	import TodoFooter from './components/TodoFooter.vue'
 
-  export default {
+  export default{
+      //데이터 속성 todoItems 선언
+  	data(){
+          return{
+              todoItems:[]
+          }
+  	},
+    created(){
+        if(localStorage.length > 0){
+            for(var i = 0; i < localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
+      methods:{
+          addTodo(todoItem){
+              //로컬 스토리지에 데이터를 추가하는 로직
+              localStorage.setItem(todoItem, todoItem);
+              this.todoItems.push(todoItem);
+          },
+          clearAll(){
+              localStorage.clear();
+              this.todoItems = [];
+          },
+          removeTodo(todoItem, index){
+              localStorage.removeItem(todoItem);
+              this.todoItems.splice(index, 1);
+          }
+      },
       components:{
           'TodoHeader' : TodoHeader,
-          'TodoInput' : TodoInput,
-          'TodoList' : TodoList,
+          'TodoInput'  : TodoInput,
+          'TodoList'   : TodoList,
           'TodoFooter' : TodoFooter
       }
   }
